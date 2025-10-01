62db365d0a5d474992871a8bd3b22ee386330909
Khurram Shahzad <khurramshahzad028@gmail.com>
2025-10-01 13:22:33 +0500

    Models : Finalized Relation of Tables

--- app/Models/course.php
+++ app/Models/course.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class course extends Model
 {
-    //
+    use HasFactory;
+    public $timestamps = false;
+    protected $primaryKey = 'course_id';
+
+    protected $fillable = [
+        'tenant_id',
+        'course_name',
+        'teacher_id',
+    ];
 }
--- app/Models/enrollment.php
+++ app/Models/enrollment.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class enrollment extends Model
 {
-    //
+    use HasFactory;
+    public $timestamps = false;
+    protected $primaryKey = 'enrollment_id';
+
+    protected $fillable = [
+        'tenant_id',
+        'course_id',
+        'student_id',
+        'enrollment_date',
+    ];
 }
